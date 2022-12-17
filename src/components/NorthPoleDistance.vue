6c2493700d5ff2f496484f0c72bac5409bcab8df
mattmaribojoc <matthewmaribojoc@gmail.com>
2022-12-17 10:57:40 -0500

    add solution

--- src/components/NorthPoleDistance.vue
+++ src/components/NorthPoleDistance.vue
@@ -3,15 +3,26 @@ import { getDistanceKm, getDistanceMiles } from '@/utils/distance'
 import { useGeolocation } from '@vueuse/core'
 import { ref, computed } from 'vue'
 
-// hint: coords.latitude + cords.latitude
 const { coords } = useGeolocation()
 
 const unit = ref<'km' | 'mile'>('mile')
 
-const toggleUnit = () => {}
+const distance = computed(() => {
+  return unit.value === 'km'
+    ? getDistanceKm(coords.value.latitude, coords.value.longitude)
+    : getDistanceMiles(coords.value.latitude, coords.value.longitude)
+})
+
+const toggleUnit = () => {
+  if (unit.value === 'km') {
+    unit.value = 'mile'
+  } else {
+    unit.value = 'km'
+  }
+}
 </script>
 
 <template>
   <!-- this should only render a slot -->
-  <slot />
+  <slot v-bind="{ unit, distance, toggleUnit }" />
 </template>
